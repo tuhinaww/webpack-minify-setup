54c76cc0a9807c414fa12cffeb6362b2452fbd61
tuhinaww <iceybubblegumtea@gmail.com>
2025-02-24 16:35:08 +0530

    feat: auto detect js and css files as well

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,39 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+const jsFiles = glob.sync("./src/**/*.js", { ignore: ["./src/index.js"] });
+const cssFiles = glob.sync("./src/**/*.css");
 const htmlFiles = glob.sync("./src/**/*.html");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+    ...Object.fromEntries(
+      jsFiles.map((file) => [
+        path.relative("./src", file).replace(/\\/g, "/").replace(/\.js$/, ""),
+        file,
+      ])
+    ),
+  },
   output: {
-    filename: "js/[name].[contenthash].min.js",
+    filename: "js/[name].min.js",
     path: path.resolve(__dirname, "dist"),
   },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: "all",
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: "css/[name].[contenthash].min.css" }),
+    new MiniCssExtractPlugin({ filename: "css/[name].min.css" }),
 
     ...htmlFiles.map((file) => {
       return new HtmlWebpackPlugin({
-        filename: path.relative("./src", file), 
+        filename: path.relative("./src", file),
         template: file,
         minify: {
           collapseWhitespace: true,
